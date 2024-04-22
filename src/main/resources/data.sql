80fa59cc0166a1af226c4eae721405853c130b74
mixanewstyle <mikhail5623@gmail.com>
2024-04-22 18:33:16 +0300

    last version

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,6 @@ INSERT INTO users (name, surname, login, password, is_banned, role_id)
 VALUES ('Nikita', 'Shmatkov', 'admin', '12345', false, 1),
        ('Andrei', 'Nikolaev', 'moderator', '12345', false, 2),
        ('Pacan', 'Chushpanov', 'user', '12345', false, 3);
+VALUES ('Nikita', 'Shmatkov', 'bildin', '123', false, 1),
+       ('Andrei', 'Nikolaev', 'gen5', '154', false, 2),
+       ('Pacan', 'Chushpanov', 'gop470', '470', false, 3);
